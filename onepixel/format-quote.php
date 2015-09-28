8b7552e08dcef231d3c9d32892a431c0e0056da2
Patrick Danhof <info@pixiebox.com>
2015-09-28 14:03:37 +0200

    line endings
    line endings to UNIX

--- onepixel/format-quote.php
+++ onepixel/format-quote.php
@@ -2,9 +2,9 @@
 $the_title = get_the_title();
 ?>
 <div class="<?php echo $width_class; ?>">
-	<div class="clearfix page-content">	 	 
+	<div class="clearfix page-content">
 		<q>
-			<?php echo get_the_content(); ?>	 	 
+			<?php echo get_the_content(); ?>
 		</q>
 	</div>
 	<?php if (!empty($the_title)) : ?>
